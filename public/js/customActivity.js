b62994fc3eb3de3040a36e01ce660e8753ef0e8e
mahendranaykodi <115994045+mahendranaykodi@users.noreply.github.com>
2022-10-28 16:11:40 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,110 +1,202 @@
-define(['postmonger'], function (Postmonger) {
-    'use strict';
+define(["postmonger"], function (Postmonger) {
+  "use strict";
 
-    let connection = new Postmonger.Session();
-    let authTokens = {};
-    let payload = {};
+  var connection = new Postmonger.Session();
+  var payload = {};
+  var lastStepEnabled = false;
+  var steps = [
+    // initialize to the same value as what's set in config.json for consistency
+    { label: "Step 1", key: "step1" },
+    { label: "Step 2", key: "step2" },
+    { label: "Step 3", key: "step3" },
+    { label: "Step 4", key: "step4", active: false },
+  ];
+  var currentStep = steps[0].key;
 
-    // Configuration variables
-    let eventSchema = ''; // variable is used in parseEventSchema()
-    let lastnameSchema = ''; // variable is used in parseEventSchema()
-    let eventDefinitionKey;
+  $(window).ready(onRender);
 
-    $(window).ready(onRender);
-    connection.on('initActivity', initialize);
-    connection.on('clickedNext', save); //Save function within MC
+  connection.on("initActivity", initialize);
+  connection.on("requestedTokens", onGetTokens);
+  connection.on("requestedEndpoints", onGetEndpoints);
 
-    function onRender() {
-        // JB will respond the first time 'ready' is called with 'initActivity'
-        connection.trigger('ready');
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
-    }
+  connection.on("clickedNext", onClickedNext);
+  connection.on("clickedBack", onClickedBack);
+  connection.on("gotoStep", onGotoStep);
 
-    /**
-     * This function is to pull out the event definition within journey builder.
-     * With the eventDefinitionKey, you are able to pull out values that passes through the journey
-     */
-    connection.trigger('requestTriggerEventDefinition');
-    connection.on('requestedTriggerEventDefinition', function (eventDefinitionModel) {
-        if (eventDefinitionModel) {
-            eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
-            // console.log('Request Trigger >>>', JSON.stringify(eventDefinitionModel));
-        }
+  function onRender() {
+    // JB will respond the first time 'ready' is called with 'initActivity'
+    connection.trigger("ready");
+
+    connection.trigger("requestTokens");
+    connection.trigger("requestEndpoints");
+
+    // Disable the next button if a value isn't selected
+    $("#select1").change(function () {
+      var message = getMessage();
+      connection.trigger("updateButton", {
+        button: "next",
+        enabled: Boolean(message),
+      });
+
+      $("#message").html(message);
     });
 
-    function initialize(data) {
-        if (data) {
-            payload = data;
+    // Toggle step 4 active/inactive
+    // If inactive, wizard hides it and skips over it during navigation
+    $("#toggleLastStep").click(function () {
+      lastStepEnabled = !lastStepEnabled; // toggle status
+      steps[3].active = !steps[3].active; // toggle active
+
+      connection.trigger("updateSteps", steps);
+    });
+  }
+
+  function initialize(data) {
+    if (data) {
+      payload = data;
+    }
+
+    var message;
+    var hasInArguments = Boolean(
+      payload["arguments"] &&
+        payload["arguments"].execute &&
+        payload["arguments"].execute.inArguments &&
+        payload["arguments"].execute.inArguments.length > 0
+    );
+
+    var inArguments = hasInArguments
+      ? payload["arguments"].execute.inArguments
+      : {};
+
+    $.each(inArguments, function (index, inArgument) {
+      $.each(inArgument, function (key, val) {
+        if (key === "message") {
+          message = val;
         }
-        initialLoad(data);
-        parseEventSchema();
+      });
+    });
+
+    // If there is no message selected, disable the next button
+    if (!message) {
+      showStep(null, 1);
+      connection.trigger("updateButton", { button: "next", enabled: false });
+      // If there is a message, skip to the summary step
+    } else {
+      $("#select1")
+        .find("option[value=" + message + "]")
+        .attr("selected", "selected");
+      $("#message").html(message);
+      showStep(null, 3);
+    }
+  }
+
+  function onGetTokens(tokens) {
+    // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+    // console.log(tokens);
+  }
+
+  function onGetEndpoints(endpoints) {
+    // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+    // console.log(endpoints);
+  }
+
+  function onClickedNext() {
+    if (
+      (currentStep.key === "step3" && steps[3].active === false) ||
+      currentStep.key === "step4"
+    ) {
+      save();
+    } else {
+      connection.trigger("nextStep");
+    }
+  }
+
+  function onClickedBack() {
+    connection.trigger("prevStep");
+  }
+
+  function onGotoStep(step) {
+    showStep(step);
+    connection.trigger("ready");
+  }
+
+  function showStep(step, stepIndex) {
+    if (stepIndex && !step) {
+      step = steps[stepIndex - 1];
     }
 
-    /**
-     * Save function is fired off upon clicking of "Done" in Marketing Cloud
-     * The config.json will be updated here if there are any updates to be done via Front End UI
-     */
-    function save() {
-        payload['arguments'].execute.inArguments = [
-            {
-                SAMPLE_PARAM: "SAMPLE PARAM DATA FROM CONFIG.JSON"
-            }
-        ];
-        payload['metaData'].isConfigured = true;
-        connection.trigger('updateActivity', payload);
-    }
+    currentStep = step;
 
-    /**
-     * 
-     * @param {*} data
-     * 
-     * This data param is the config json payload that needs to be loaded back into the UI upon opening the custom application within journey builder 
-     * This function is invoked when the user clicks on the custom activity in Marketing Cloud. 
-     * If there are information present, it should be loaded back into the appropriate places. 
-     * e.g. input fields, select lists
-     */
-    function initialLoad(data) {
-    };
+    $(".step").hide();
 
-
-    /**
-     * This function is to pull the relevant information to create the schema of the objects
-     * 
-     * This function pulls out the schema for additional customizations that can be used.
-     * This function leverages on the required field of "Last Name" to pull out the overall event schema
-     * 
-     * returned variables of: lastnameSchema , eventSchema.
-     * eventSchema = Case:Contact:
-     * lastnameSchema = Case:Contact:<last_name_schema>
-     * 
-     * View the developer console in chrome upon opening of application in MC Journey Builder for further clarity.
-     */
-    function parseEventSchema() {
-        // Pulling data from the schema
-        connection.trigger('requestSchema');
-        connection.on('requestedSchema', function (data) {
-            // save schema
-            let dataJson = data['schema'];
-
-            for (let i = 0; i < dataJson.length; i++) {
-
-                // Last name schema and creation of event schema
-                // Last name is a required field in SF so this is used to pull the event schema
-                if (dataJson[i].key.toLowerCase().replace(/ /g, '').indexOf("lastname") !== -1) {
-                    let splitArr = dataJson[i].key.split(".");
-                    lastnameSchema = splitArr[splitArr.length - 1];
-                    console.log('Last Name Schema >>', lastnameSchema);
-
-                    let splitName = lastnameSchema.split(":");
-                    let reg = new RegExp(splitName[splitName.length - 1], "g");
-                    let oldSchema = splitArr[splitArr.length - 1];
-
-                    eventSchema = oldSchema.replace(reg, "");
-                    console.log("Event Schema >>", eventSchema);
-                }
-            }
-
+    switch (currentStep.key) {
+      case "step1":
+        $("#step1").show();
+        connection.trigger("updateButton", {
+          button: "next",
+          enabled: Boolean(getMessage()),
         });
+        connection.trigger("updateButton", {
+          button: "back",
+          visible: false,
+        });
+        break;
+      case "step2":
+        $("#step2").show();
+        connection.trigger("updateButton", {
+          button: "back",
+          visible: true,
+        });
+        connection.trigger("updateButton", {
+          button: "next",
+          text: "next",
+          visible: true,
+        });
+        break;
+      case "step3":
+        $("#step3").show();
+        connection.trigger("updateButton", {
+          button: "back",
+          visible: true,
+        });
+        if (lastStepEnabled) {
+          connection.trigger("updateButton", {
+            button: "next",
+            text: "next",
+            visible: true,
+          });
+        } else {
+          connection.trigger("updateButton", {
+            button: "next",
+            text: "done",
+            visible: true,
+          });
+        }
+        break;
+      case "step4":
+        $("#step4").show();
+        break;
     }
+  }
+
+  function save() {
+    var name = $("#select1").find("option:selected").html();
+    var value = getMessage();
+
+    // 'payload' is initialized on 'initActivity' above.
+    // Journey Builder sends an initial payload with defaults
+    // set by this activity's config.json file.  Any property
+    // may be overridden as desired.
+    payload.name = name;
+
+    payload["arguments"].execute.inArguments = [{ message: value }];
+
+    payload["metaData"].isConfigured = true;
+
+    connection.trigger("updateActivity", payload);
+  }
+
+  function getMessage() {
+    return $("#select1").find("option:selected").attr("value").trim();
+  }
 });
